193844f563d0f62283c3c1506d79008e74372a53
walterISF <wisf_94@hotmail.com>
2016-12-01 22:12:10 -0200

    Rotas Mapa

--- app/src/main/java/br/com/cloudbank/inacio/walter/cloudbank/DetalheFatura.java
+++ app/src/main/java/br/com/cloudbank/inacio/walter/cloudbank/DetalheFatura.java
@@ -11,6 +11,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -32,8 +33,16 @@ public class DetalheFatura extends AppCompatActivity implements OnMapReadyCallba
     @Override
     public void onMapReady(GoogleMap googleMap) {
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng PUC = new LatLng(-22.8336965, -47.0532537);
+        googleMap.addMarker(new MarkerOptions().position(PUC).title("PUC Campinas"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(PUC));
+        googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(PUC)      // Sets the center of the map to Mountain View
+                .zoom(17)                   // Sets the zoom
+                .bearing(90)                // Sets the orientation of the camera to east
+                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
+                .build();                   // Creates a CameraPosition from the builder
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 }
